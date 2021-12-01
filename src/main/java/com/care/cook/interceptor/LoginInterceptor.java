0929adcc6ce3b63e6364a80372f203949ee8adc1
raina <skgus5598@gmail.com>
2021-12-01 17:47:29 +0900

    인터셉터 테스트

---
+++ src/main/java/com/care/cook/interceptor/LoginInterceptor.java
@@ -0,0 +1,17 @@
+package com.care.cook.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("로그인 인터셉터 동작 확인 ");
+		return true;
+	}
+
+}
